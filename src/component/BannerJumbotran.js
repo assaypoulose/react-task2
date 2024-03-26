366933cbe09d71ce3fc8ed9713c08f778f26f8bd
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-03-26 17:00:57 +0100

    Add files via upload

---
+++ src/component/BannerJumbotran.js
@@ -0,0 +1,14 @@
+function Banner() {
+    return (<>
+        <div className="container-fluid bg-dark text-white p-5" style={{height:"170px", margin:'0px', padding:"0px"}}>
+            <div className="jumbotron p-md-3 p-2 ">
+                <h1>E-Commerce CART</h1>
+                <h2>Shop in style</h2>
+                <p>With this shop hompeage template</p>
+            </div>
+
+        </div>
+    </>);
+}
+
+export default Banner;
